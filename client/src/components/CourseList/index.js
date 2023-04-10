f77cfa7267facf46117ee0da36e0a33f51e3f049
WGT <williamgeorgethomas@gmail.com>
2023-04-09 21:26:41 -0500

    updated map in CourseList to what i think it should be to render courses

--- client/src/components/CourseList/index.js
+++ client/src/components/CourseList/index.js
@@ -1,30 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CourseList = ({ profiles, name }) => {
-//   if (!profiles.length) {
-//     return <h3>No Profiles Yet</h3>;
-//   }
+const CourseList = ({ courses, name }) => {
 
   return (
     <div className="bg-[#FDF0D5]">
     <h2 className="stylish text-3xl font-bold ml-8">courses will display here</h2>
       <h3 className="">{name}</h3>
       <div className="flex-row justify-space-between my-4">
-        {profiles &&
-          profiles.map((profile) => (
-            <div key={profile._id} className="">
+        {courses &&
+          courses.map((course) => (
+            <div key={course._id} className="">
               <div className="">
                 <h4 className="">
-                  {profile.name} <br />
+                  {course.name} <br />
                   <span className="" style={{ fontSize: "1rem" }}>
-                    currently has {profile.skills ? profile.skills.length : 0} endorsed skill
-                    {profile.skills && profile.skills.length === 1 ? "" : "s"}
+                    currently has {course.students ? course.students : 0} students enrolled
+                    {course.students && course.students === 1 ? "" : "s"}
                   </span>
                 </h4>
 
-                <Link className="" to={`/profiles/${profile._id}`}>
-                  View and endorse their skills.
+                <Link className="" to={`/courses/${course._id}`}>
+                  Learn more & enroll!
                 </Link>
               </div>
             </div>
