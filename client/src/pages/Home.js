4461ea7d6d890cb6adac85c938ab8af2a78ca917
Miree Zeidan <mireezeidan@gmail.com>
2023-04-10 21:37:44 -0500

    created course cards on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex-row justify-center">
+      <div className="flex-col justify-center">
         <div className="col-12 col-md-10 my-3">
           {loading ? (
             <div>Loading...</div>
