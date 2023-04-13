4a5e32f50c8139ec3de09ccd07d745cfa68a9986
WGT <williamgeorgethomas@gmail.com>
2023-04-12 19:59:44 -0500

    more styling and disabled first option in Search By Instrument. new text in About component

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,10 +6,12 @@ const About = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center">
-      <h1 className="stylish text-3xl font-bold">What is 5th Octave?</h1>
-      <div className="text-center stylish text-xl w-1/2">We are musicians who are passionate about using the power of technology to connect the best music teachers with students anywhere in the world</div>
-      <h2 className="stylish text-2xl font-bold">How it works</h2>
-      <div className="stylish text-xl">search classes, group/individual classes, video call, blah blah blah</div>
+      <h1 className="stylish text-3xl font-bold text-[#669BBC]">What is 5th Octave?</h1>
+      <div className="text-center stylish text-xl w-1/2 font-semibold">We are musicians who are passionate about using the power of technology to connect the best music teachers with students anywhere in the world</div>
+      <h2 className="stylish text-2xl font-bold text-[#669BBC]">How it works</h2>
+      <div className="stylish text-xl text-center font-semibold">
+        Search by instrument. Enroll in a group class.<br></br> Learn online in the privacy of your home.<br></br> Experience the joy of learning a new instrument!
+      </div>
     </div>
   );
 };
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -26,7 +26,9 @@ const Header = () => {
           <button className="border-4 border-[#FDF0D5] rounded-lg bg-[#C1121F] p-2 text-[#FDF0D5] stylish">Search</button>
         </form> */}
         <select className="my-2 p-2 rounded-lg border-4 border-[#C1121F] bg-[#FDF0D5] stylish font-bold text-lg" onChange={instrumentFilter}>
-          <option value="">Filter By Instrument</option>
+          <option selected disabled>
+            Search By Instrument
+          </option>
           <option value="Piano">Piano</option>
           <option value="Guitar">Guitar</option>
           <option value="Trumpet">Trumpet</option>
